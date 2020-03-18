bf3ab53c36ea2482f2ad7216b7e305e0788ca38b
mccalluc <mccalluc@users.noreply.github.com>
2020-03-18 16:53:48 -0400

    Generate JSON schema from Table Schema

--- src/generate.py
+++ src/generate.py
@@ -4,7 +4,7 @@ import argparse
 from pathlib import Path
 import sys
 
-from yaml import safe_load as load_yaml
+from yaml import safe_load as load_yaml, dump as dump_yaml
 
 _schemas_path = (
     Path(__file__).parent / 'hubmap_ingest_validator' / 'table-schemas'
@@ -16,18 +16,39 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'type', metavar='TYPE', type=str,
+        'type',
         choices=valid_types,
-        help='Template to generate')
+        help='What type to generate for')
+    parser.add_argument(
+        'target',
+        choices=['template', 'schema'],
+        help='What kind of thing to generate')
     args = parser.parse_args()
-    print(_generate_csv_template(args.type))
+    if args.target == 'template':
+        print(_generate_csv_template(args.type))
+    elif args.target == 'schema':
+        print(_generate_json_schema(args.type))
 
 
 def _generate_csv_template(type):
     schema_path = _schemas_path / f'{type}.yaml'
-    schema = load_yaml(open(schema_path).read())
-    names = [field['name'] for field in schema['fields']]
+    table_schema = load_yaml(open(schema_path).read())
+    names = [field['name'] for field in table_schema['fields']]
     return '\t'.join(names) + '\n'
+
+
+def _generate_json_schema(type):
+    schema_path = _schemas_path / f'{type}.yaml'
+    table_schema = load_yaml(open(schema_path).read())
+    json_schema = {
+        'properties': {
+            field['name']: {
+                'type': 'string',  # TODO: for now...
+                'description': field['description']
+            } for field in table_schema['fields']
+        }
+    }
+    return dump_yaml(json_schema)
 
 
 if __name__ == "__main__":
