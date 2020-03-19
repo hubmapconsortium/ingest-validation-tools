ff5d3ac245314a617c3586907ce0d7cfd02b13e1
mccalluc <mccalluc@users.noreply.github.com>
2020-03-19 14:26:25 -0400

    stub metadata validation

--- src/hubmap_ingest_validator/validator.py
+++ src/hubmap_ingest_validator/validator.py
@@ -9,9 +9,10 @@ def validate(path, type):
     path_obj = Path(path)
     _validate_generic_submission(path_obj)
     _validate_dataset_directories(path_obj, type)
+    _validate_metadata_csv(path_obj / 'metadata.txt')
 
 
-def _validate_generic_submission(path):
+def _validate_generic_submission(dir_path):
     '''
     Validate the directory at path.
     '''
@@ -19,10 +20,10 @@ def _validate_generic_submission(path):
     schema_path = (Path(__file__).parent
                    / 'directory-schemas' / 'submission.yaml')
     schema = load_yaml(open(schema_path).read())
-    directory_schema.validate_dir(path, schema)
+    directory_schema.validate_dir(dir_path, schema)
 
 
-def _validate_dataset_directories(path, type):
+def _validate_dataset_directories(dir_path, type):
     '''
     Validate the subdirectories under path as type.
     '''
@@ -30,6 +31,10 @@ def _validate_dataset_directories(path, type):
     schema_path = (Path(__file__).parent
                    / 'directory-schemas' / 'datasets' / f'{type}.yaml')
     schema = load_yaml(open(schema_path).read())
-    for sub_directory in [sd for sd in path.iterdir() if sd.is_dir()]:
+    for sub_directory in [sd for sd in dir_path.iterdir() if sd.is_dir()]:
         logging.info(f'  Validating {sub_directory}...')
         directory_schema.validate_dir(sub_directory, schema)
+
+
+def _validate_metadata_csv(metadata_path):
+    pass
