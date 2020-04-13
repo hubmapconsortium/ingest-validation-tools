97f1ba3a1d722e109c656d3df92351c0c02f11dd
Chuck McCallum <mccalluc@users.noreply.github.com>
2020-04-13 13:22:28 -0400

    Mccalluc/globus (#157)
    * cache dir
    
    * globus cache works!
    
    * fix tests

--- src/ingest_validation_tools/globus_utils.py
+++ src/ingest_validation_tools/globus_utils.py
@@ -1,4 +1,5 @@
 import subprocess
+from pathlib import Path
 
 
 def get_globus_connection_error(origin):
@@ -11,9 +12,37 @@ def get_globus_connection_error(origin):
 
     try:
         subprocess.run(
-            ['globus', 'ls', origin], check=True,
+            ['globus', 'ls', origin], check=True, text=True,
             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
-        return e.stdout.decode('utf-8')
+        return e.stdout
 
     return None
+
+
+def get_globus_cache_path(origin, path):
+    target = _get_cache_target(origin, path)
+    if not target.exists():
+        _cache_globus_origin_path(origin, path)
+    return target
+
+
+def _cache_globus_origin_path(origin, path):
+    result = subprocess.run(
+        ['globus', 'ls', '--recursive', f'{origin}:{path}'],
+        check=True, text=True, capture_output=True)
+    listing = result.stdout.split('\n')
+    target = _get_cache_target(origin, path)
+    for globus_path in listing:
+        local_path = target / globus_path
+        if globus_path.endswith('/'):
+            local_path.mkdir(parents=True)
+        else:
+            local_path.touch()
+            # Download *-metadata.tsv.
+
+
+def _get_cache_target(origin, path):
+    if path[0] == '/':
+        path = path[1:]
+    return Path(__file__).parent / 'globus-cache' / origin / path
