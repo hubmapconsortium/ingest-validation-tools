230715052f9de69938e6df6d54fd289224f45239
mccalluc <mccalluc@users.noreply.github.com>
2020-04-22 17:50:23 -0400

    _add_constraints helper function

--- src/ingest_validation_tools/table_schema_loader.py
+++ src/ingest_validation_tools/table_schema_loader.py
@@ -21,33 +21,7 @@ def get_schema(type, optional_fields=[]):
 
     fields = level_1_fields + type_fields + paths_fields
     for field in fields:
-        if 'constraints' not in field:
-            field['constraints'] = {}
-
-        # Guess constraints:
-        if 'required' not in field['constraints']:
-            field['constraints']['required'] = True
-        if 'protocols_io_doi' in field['name']:
-            field['constraints']['pattern'] = r'10\.17504/.*'
-        if field['name'].endswith('_email'):
-            field['format'] = 'email'
-
-        # Guess types:
-        if field['name'].startswith('is_'):
-            field['type'] = 'boolean'
-        if field['name'].endswith('_value'):
-            field['type'] = 'number'
-        if field['name'].startswith('number_of_'):
-            field['type'] = 'integer'
-        if 'percent' in field['name']:
-            field['type'] = 'number'
-            field['constraints']['minimum'] = 0
-            field['constraints']['maximum'] = 100
-
-        # Override:
-        if field['name'] in optional_fields:
-            field['constraints']['required'] = False
-
+        _add_constraints(field, optional_fields)
     return {
         'doc_url': type_schema['doc_url'],
         'fields': fields
@@ -56,3 +30,53 @@ def get_schema(type, optional_fields=[]):
 
 def _get_sub_schema(type):
     return load_yaml(open(_schemas_path / f'{type}.yaml').read())
+
+
+def _add_constraints(field, optional_fields):
+    '''
+    Modifies field in-place
+
+    >>> from pprint import pprint
+    >>> field = {'name': 'abc_percent'}
+    >>> _add_constraints(field, [])
+    >>> pprint(field, width=40)
+    {'constraints': {'maximum': 100,
+                     'minimum': 0,
+                     'required': True},
+     'name': 'abc_percent',
+     'type': 'number'}
+
+    >>> field = {'name': 'optional_value'}
+    >>> _add_constraints(field, ['optional_value'])
+    >>> pprint(field, width=40)
+    {'constraints': {'required': False},
+     'name': 'optional_value',
+     'type': 'number'}
+
+    '''
+    if 'constraints' not in field:
+        field['constraints'] = {}
+
+    # Guess constraints:
+    if 'required' not in field['constraints']:
+        field['constraints']['required'] = True
+    if 'protocols_io_doi' in field['name']:
+        field['constraints']['pattern'] = r'10\.17504/.*'
+    if field['name'].endswith('_email'):
+        field['format'] = 'email'
+
+    # Guess types:
+    if field['name'].startswith('is_'):
+        field['type'] = 'boolean'
+    if field['name'].endswith('_value'):
+        field['type'] = 'number'
+    if field['name'].startswith('number_of_'):
+        field['type'] = 'integer'
+    if 'percent' in field['name']:
+        field['type'] = 'number'
+        field['constraints']['minimum'] = 0
+        field['constraints']['maximum'] = 100
+
+    # Override:
+    if field['name'] in optional_fields:
+        field['constraints']['required'] = False
