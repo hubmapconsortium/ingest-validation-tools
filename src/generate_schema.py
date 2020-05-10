fc5f9d740911eb1cd3b4902525c68e649dda9c5c
mccalluc <mccalluc@users.noreply.github.com>
2020-05-10 14:15:57 -0400

    helper script

---
+++ src/generate_schema.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+import argparse
+
+from yaml import dump as dump_yaml
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--fields',
+        type=argparse.FileType('r'),
+        help='Two-column TSV: Field name and description')
+    args = parser.parse_args()
+
+    field_list = []
+    for row in csv.reader(args.fields, dialect='excel-tab'):
+        if len(row) == 2:
+            field_list.append({
+                'name': row[0],
+                'description': row[1]
+            })
+    field_list[0] = {
+        # Rebuild dict, so 'heading' is first.
+        'heading': 'Level 2',
+        **field_list[0]
+    }
+    print(dump_yaml({
+        'doc_url': 'TODO',
+        'fields': field_list
+    }, sort_keys=False))
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
