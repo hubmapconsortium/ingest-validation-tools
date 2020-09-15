f1af86b3770ef569d650c8d902093cfbe2e1a90c
mccalluc <mccalluc@users.noreply.github.com>
2020-09-15 16:23:45 -0400

    Check if types are aligned

---
+++ test-aligned-assay-types.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import sys
+from pathlib import Path
+from yaml import safe_load as load_yaml
+
+
+def get_search_api_assay_types():
+    assay_types_path = (
+        Path(__file__).parent /
+        'src/search-api/src/search-schema/data' /
+        'definitions/enums/assay_types.yaml')
+    assay_types_info = load_yaml(assay_types_path.read_text())
+    return [v['description'] for v in assay_types_info.values()]
+
+
+def get_level_1_assay_types():
+    level_1_path = (
+        Path(__file__).parent /
+        'src/ingest_validation_tools/table-schemas' /
+        'level-1.yaml')
+    level_1_info = load_yaml(level_1_path.read_text())
+    assay_type_matches = [
+        f for f in level_1_info['fields']
+        if f['name'] == 'assay_type'
+    ]
+    assert len(assay_type_matches) == 1
+    return assay_type_matches[0]['constraints']['enum']
+
+
+def main():
+    search_api_assays = set(get_search_api_assay_types())
+    level_1_assays = set(get_level_1_assay_types())
+    search_api_but_not_level_1 = search_api_assays - level_1_assays
+    level_1_but_not_search_api = level_1_assays - search_api_assays
+    if search_api_but_not_level_1 or level_1_but_not_search_api:
+        print(f'search-api but not level-1: {search_api_but_not_level_1}')
+        print(f'level-1 but not search-api: {level_1_but_not_search_api}')
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
