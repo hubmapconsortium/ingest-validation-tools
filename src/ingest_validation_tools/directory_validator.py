924613493d9d18f8c2ae6c978db57195d70ca17d
mccalluc <mccalluc@users.noreply.github.com>
2020-09-30 15:52:42 -0400

    Add newlines

--- src/ingest_validation_tools/directory_validator.py
+++ src/ingest_validation_tools/directory_validator.py
@@ -3,6 +3,7 @@ import re
 from fnmatch import fnmatch
 from importlib import import_module
 
+
 def default_factory(base_path, assay_type):
     """
     default validator factory
@@ -30,6 +31,7 @@ class DefaultDirectoryContentValidator(DirectoryContentValidator):
     """
     Implements backward compatible calls to content validation modules
     """
+
     def collect_errors(self):
         validator = import_module(
             f'ingest_validation_tools.content_validation.{self.assay_type}')
