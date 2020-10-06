8adf0797d8a72670d24673efb3261008116d1189
mccalluc <mccalluc@users.noreply.github.com>
2020-10-06 12:23:29 -0400

    Validating .gz files

--- src/ingest_validation_tools/directory_content_validator.py
+++ src/ingest_validation_tools/directory_content_validator.py
@@ -1,7 +1,22 @@
-def _validator_factory(base_path, assay_type):
+from os import walk
+from pathlib import Path
+
+
+def _directory_validator_factory(base_path, assay_type):
     # TODO: Create other Validator subclasses, and return them instead,
     # depending on assay_type.
     return DirectoryContentValidator(base_path, assay_type)
+
+
+def _get_file_start_error(file_path):
+    suffix = file_path.suffix
+    if suffix == '.gz':
+        with file_path.open('rb') as f:
+            bytes = f.read(2)
+            expected = b'\x1F\x8B'
+            if bytes != expected:
+                return f'Unexpected bytes ({bytes}) at start of .gz file'
+    return None
 
 
 class DirectoryContentValidator(object):
@@ -9,12 +24,30 @@ class DirectoryContentValidator(object):
         self.base_path = base_path
         self.assay_type = assay_type
 
+    def collect_file_errors(self):
+        errors = {}
+        for root, _dirs, files in walk(self.base_path):
+            for f in files:
+                file_path = Path(root) / f
+                start_error = _get_file_start_error(file_path)
+                if start_error:
+                    errors[str(file_path)] = start_error
+        return errors
+
+    def collect_other_errors(self):
+        return {}
+
     def collect_errors(self):
-        """
-        Subclasses should return an array listing the errors,
-        or if more structure is desired, a dict.
-        """
-        return []
+        errors = {}
+        file_errors = self.collect_file_errors()
+        if file_errors:
+            errors['File content'] = file_errors
+
+        other_errors = self.collect_other_errors()
+        if other_errors:
+            errors['Other'] = other_errors
+
+        return errors
 
 
 class DirectoryContentValidationErrors(Exception):
@@ -23,7 +56,7 @@ class DirectoryContentValidationErrors(Exception):
 
 
 def validate_directory_content(assay_type, data_path):
-    validator = _validator_factory(data_path, assay_type)
+    validator = _directory_validator_factory(data_path, assay_type)
     errors = validator.collect_errors()
     if errors:
         raise DirectoryContentValidationErrors(errors)
