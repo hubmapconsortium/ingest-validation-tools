0c27e6fd74c3de5f4676c94bcc4ebe207cfa1383
Joel Welling <welling@psc.edu>
2020-10-06 10:49:05 -0400

    Welling/arbitrary validation code (#372)
    * Added a factory function as a hook to replace content validation
    
    * Reorder code in directory_validator to avoid late definition
    
    * Add newlines
    
    Co-authored-by: mccalluc <mccalluc@users.noreply.github.com>

--- src/ingest_validation_tools/directory_validator.py
+++ src/ingest_validation_tools/directory_validator.py
@@ -4,6 +4,40 @@ from fnmatch import fnmatch
 from importlib import import_module
 
 
+def default_factory(base_path, assay_type):
+    """
+    default validator factory
+    """
+    return DefaultDirectoryContentValidator(base_path, assay_type)
+
+
+DIRECTORY_CONTENT_VALIDATOR_FACTORY = default_factory
+
+
+class DirectoryContentValidator(object):
+    def __init__(self, base_path, assay_type):
+        self.base_path = base_path
+        self.assay_type = assay_type
+
+    def collect_errors(self):
+        """
+        returns string or None.  Multiple errors are to be reported
+        via a single multi-line string.
+        """
+        return None
+
+
+class DefaultDirectoryContentValidator(DirectoryContentValidator):
+    """
+    Implements backward compatible calls to content validation modules
+    """
+
+    def collect_errors(self):
+        validator = import_module(
+            f'ingest_validation_tools.content_validation.{self.assay_type}')
+        return validator.collect_errors(self.base_path)
+
+
 class DirectoryValidationErrors(Exception):
     def __init__(self, errors):
         self.errors = errors
@@ -72,8 +106,7 @@ def _get_required_allowed(dir_schema):
 
 
 def validate_directory_content(assay_type, data_path):
-    validator = import_module(
-        f'ingest_validation_tools.content_validation.{assay_type}')
-    errors = validator.collect_errors(data_path)
+    validator = DIRECTORY_CONTENT_VALIDATOR_FACTORY(data_path, assay_type)
+    errors = validator.collect_errors()
     if errors:
         raise DirectoryValidationErrors(errors)
