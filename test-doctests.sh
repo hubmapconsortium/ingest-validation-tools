12c34ac1061cbc711d1dd9cf0618524091103aac
mccalluc <mccalluc@users.noreply.github.com>
2020-11-09 12:28:21 -0500

    Fix doctest... Globus functionality may have been broken?

---
+++ test-doctests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -o errexit
+
+cd src
+
+DOCTESTS=`find . | grep -v ingest-validation-tests | grep '\.py$'`
+for TEST in $DOCTESTS; do
+  CMD="python -m doctest $TEST"
+  # doctest by itself is silent on success.
+  echo 'cd src;' $CMD
+  $CMD
+done
+
+cd -
