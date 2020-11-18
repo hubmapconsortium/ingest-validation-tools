7c6ddb49d7161008664d19b61a6c37768cbcf93b
mccalluc <mccalluc@users.noreply.github.com>
2020-11-18 14:04:10 -0500

    Parameterize other

--- test-other-generate.sh
+++ test-other-generate.sh
@@ -5,14 +5,16 @@ red=`tput setaf 1`
 reset=`tput sgr0`
 die() { set +v; echo "$red$*$reset" 1>&2 ; exit 1; }
 
-REAL_DEST="docs/sample"
-TEST_DEST="docs-test/sample"
+for TARGET in 'sample'; do
+  REAL_DEST="docs/$TARGET"
+  TEST_DEST="docs-test/$TARGET"
 
-REAL_CMD="src/generate_other_docs.py $REAL_DEST"
-TEST_CMD="src/generate_other_docs.py $TEST_DEST"
+  REAL_CMD="src/generate_other_docs.py $REAL_DEST"
+  TEST_CMD="src/generate_other_docs.py $TEST_DEST"
 
-mkdir -p docs-test/sample || echo "Already exists"
-eval $TEST_CMD
-diff -r $REAL_DEST $TEST_DEST \
-  || die "Update needed: $REAL_CMD"
-rm -rf docs-test
+  mkdir -p $TEST_DEST || echo "Already exists"
+  eval $TEST_CMD
+  diff -r $REAL_DEST $TEST_DEST \
+    || die "Update needed: $REAL_CMD"
+  rm -rf docs-test
+done
