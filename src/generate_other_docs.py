7715ef70bf87662f67c176dc7728af9b14c6a56b
mccalluc <mccalluc@users.noreply.github.com>
2020-11-18 15:23:50 -0500

    Just contributors.tsv

--- src/generate_other_docs.py
+++ src/generate_other_docs.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import sys
 
 from ingest_validation_tools.docs_utils import (
-    get_tsv_name, generate_template_tsv, generate_readme_md)
+    generate_template_tsv, generate_readme_md)
 from ingest_validation_tools.schema_loader import (
     get_other_schema)
 from ingest_validation_tools.argparse_types import dir_path
@@ -25,7 +25,7 @@ def main():
 
     table_schema = get_other_schema(args.type)
 
-    (Path(args.target) / get_tsv_name(args.type)).write_text(
+    (Path(args.target) / f'{args.type}.tsv').write_text(
         generate_template_tsv(
             table_schema))
     (Path(args.target) / 'README.md').write_text(
