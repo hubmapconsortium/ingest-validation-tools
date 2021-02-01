92f9a8a8de999c7a84730b6fcd6ff6f728827f0b
mccalluc <mccalluc@users.noreply.github.com>
2021-02-01 14:25:49 -0500

    Start white space helper

---
+++ src/cleanup_white_space.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+import argparse
+
+from yaml import dump as dump_yaml
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--tsv_path',
+        type=argparse.FileType('r', encoding='utf-8'),
+        help='TSV to strip padding whitespace from')
+    args = parser.parse_args()
+
+    dialect = 'excel-tab'
+    writer = csv.writer(sys.stdout, dialect=dialect)
+
+    # There could be whitespace inside the quoted value,
+    # so we really do need to parse it as a tsv.
+    for row in csv.reader(args.tsv_path, dialect=dialect):
+        writer.writerow(val.strip() for val in row)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
