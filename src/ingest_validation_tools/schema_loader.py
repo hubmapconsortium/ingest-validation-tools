edae276f06ebaffcbc7a609e4b102a5ca11f777d
mccalluc <mccalluc@users.noreply.github.com>
2021-02-25 11:51:00 -0500

    Remove complicated override logic

--- src/ingest_validation_tools/schema_loader.py
+++ src/ingest_validation_tools/schema_loader.py
@@ -40,23 +40,17 @@ def get_directory_schema(directory_type):
 
 
 def get_table_schema(table_type, optional_fields=[], offline=None):
-    level_1_fields = _get_level_1_schema('level-1')['fields']
-    paths_fields = _get_level_1_schema('paths')['fields']
     type_schema = _get_level_2_schema(table_type)
     type_fields = type_schema['fields']
+    paths_fields = _get_level_1_schema('paths')['fields']
 
-    (level_1_fields_plus_overrides, type_fields_minus_overrides) = \
-        _apply_overrides(level_1_fields, type_fields)
+    fields = type_fields + paths_fields
 
-    fields = (
-        level_1_fields_plus_overrides
-        + type_fields_minus_overrides
-        + paths_fields
-    )
     for field in fields:
+        _add_description(field)
         _add_constraints(field, optional_fields, offline=offline)
-    for field in fields:
         _validate_field(field)
+        # TODO: Check level-1 enums
 
     table_schema = {'fields': fields}
     if 'doc_url' in type_schema:
@@ -71,59 +65,25 @@ def _validate_field(field):
         raise Exception('"_unit" fields must have enum constraints', field)
 
 
+def _add_description(field):
+    descriptions = {
+        'assay_category': 'Each assay is placed into one of the following 3 general categories: '
+        'generation of images of microscopic entities, identification & quantitation of molecules '
+        'by mass spectrometry, and determination of nucleotide sequence.',
+        'assay_type': 'The specific type of assay being executed.',
+        'analyte_class': 'Analytes are the target molecules being measured with the assay.',
+    }
+    name = field['name']
+    if name in descriptions:
+        field['description'] = descriptions[name]
+
+
 def _get_level_1_schema(type):
     return load_yaml(_table_schemas_path / f'{type}.yaml')
 
 
 def _get_level_2_schema(type):
     return load_yaml(_table_schemas_path / 'level-2' / f'{type}.yaml')
-
-
-def _apply_overrides(high_fields, low_fields):
-    '''
-    Given two lists of fields, find fields with the same names in both,
-    and add the defintions from low_fields to high_fields,
-    and return the new modified high_fields, and low_fields,
-    without the fields which were there just to override.
-
-    >>> a, b = _apply_overrides(
-    ...  [{'name': 'A', 'type': '???'}],
-    ...  [{'name': 'A', 'type': '!!!'}, {'name': 'B'}]
-    ... )
-
-    >>> a
-    [{'name': 'A', 'type': '!!!'}]
-
-    >>> b
-    [{'name': 'B'}]
-
-    '''
-    high_field_names = {field['name'] for field in high_fields}
-    low_field_names = {field['name'] for field in low_fields}
-    override_field_names = set.intersection(
-        high_field_names, low_field_names)
-
-    override_fields_dict = {
-        name: [f for f in low_fields if f['name'] == name][0]
-        for name in override_field_names
-    }
-
-    _check_enum_consistency(high_fields, override_fields_dict)
-
-    high_fields_plus_overrides = [
-        (
-            # TODO: Python 3.9 will add "|" for dict merging.
-            {**field, **override_fields_dict[field['name']]}
-            if field['name'] in override_field_names
-            else field
-        )
-        for field in high_fields
-    ]
-    low_fields_minus_overrides = [
-        field for field in low_fields
-        if field['name'] not in override_field_names
-    ]
-    return high_fields_plus_overrides, low_fields_minus_overrides
 
 
 def _check_enum_consistency(high_fields, override_fields_dict):
