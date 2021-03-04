a9affa23efb469413a50e13b25030939e08b0c01
mccalluc <mccalluc@users.noreply.github.com>
2021-03-04 11:50:57 -0500

    Add argparse

--- src/generate_field_enum_csv.py
+++ src/generate_field_enum_csv.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 import sys
 from csv import DictWriter
+import argparse
 
 from ingest_validation_tools.schema_loader import (
-    list_types, get_table_schema, get_other_schema
+    list_types, get_table_schema
 )
 
 
 def main():
-    fieldnames = ['filename', 'field', 'description', 'enum']
-    writer = DictWriter(sys.stdout, fieldnames=fieldnames)
+    default_fields = ['filename', 'field', 'description', 'enum']
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--fields',
+        default=default_fields,
+        nargs='+',
+        metavar='FIELD',
+        help=f'Fields to include in report. Default: {default_fields}')
+    args = parser.parse_args()
+
+    writer = DictWriter(sys.stdout, fieldnames=args.fields)
     writer.writeheader()
     for assay_type in list_types():
         filename = f'{assay_type}.yaml'
