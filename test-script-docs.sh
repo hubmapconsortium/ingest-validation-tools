767c8b94bb981a966d9a133fa2a822f8c0c7ec97
mccalluc <mccalluc@users.noreply.github.com>
2021-03-16 15:53:53 -0400

    move script docs to sub-directory

--- test-script-docs.sh
+++ test-script-docs.sh
@@ -3,6 +3,7 @@ set -o errexit
 
 die() { set +v; echo "$red$*$reset" 1>&2 ; exit 1; }
 
+DOCS='script-docs'
 for TOOL in src/*.py; do
   TOOL=`basename $TOOL`
 
@@ -13,7 +14,7 @@ for TOOL in src/*.py; do
   echo "Testing $TOOL docs..."
   [ -e src/$TOOL ] || die "src/$TOOL does not exist."
   diff \
-        <(perl -ne 'print if /usage: '$TOOL'/../```/ and ! /```/' README-$TOOL.md) \
+        <(perl -ne 'print if /usage: '$TOOL'/../```/ and ! /```/' $DOCS/README-$TOOL.md) \
         <(src/$TOOL -h) \
-      || die 'Update: (echo '"'"'```'"'"'; src/'$TOOL' -h; echo '"'"'```'"'"') >' README-$TOOL.md
+      || die 'Update: (echo '"'"'```'"'"'; src/'$TOOL' -h; echo '"'"'```'"'"') >' $DOCS/README-$TOOL.md
 done
