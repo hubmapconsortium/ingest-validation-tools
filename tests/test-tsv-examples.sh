afe0f22382dce55e791a8514a787703c7ee4c724
mccalluc <mccalluc@users.noreply.github.com>
2021-05-26 17:25:43 -0400

    fill in the fixtures

--- tests/test-tsv-examples.sh
+++ tests/test-tsv-examples.sh
@@ -9,17 +9,17 @@ for SCHEMA in antibodies contributors sample metadata; do
   for EXAMPLE in examples/tsv-examples/$SCHEMA/*; do
     echo "Testing $EXAMPLE ..."
     CMD="src/validate_tsv.py --path $EXAMPLE/$SCHEMA.tsv --schema $SCHEMA --output as_md"
-    echo "( $CMD )"
+    echo "Running: $CMD"
     README="$EXAMPLE/README.md"
     diff $README <( eval "$CMD" ) \
       || die "Update example: $CMD > $README"
   done
 done
 
-for GOOD in examples/sample-examples/*/good-*/README.md; do
+for GOOD in examples/tsv-examples/*/good-*/README.md; do
   grep 'No errors!' $GOOD > /dev/null || die "$GOOD should not be an error report."
 done
 
-for BAD in examples/sample-examples/*/bad-*/README.md; do
+for BAD in examples/tsv-examples/*/bad-*/README.md; do
   ! grep 'No errors!' $BAD || die "$BAD should be an error report."
 done
