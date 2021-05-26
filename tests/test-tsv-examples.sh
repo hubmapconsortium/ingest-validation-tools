6d377863c919bb262b3448033698ec1eb25b309f
mccalluc <mccalluc@users.noreply.github.com>
2021-05-26 14:29:06 -0400

    rearrange fixtures

---
+++ tests/test-tsv-examples.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -o errexit
+
+red=`tput setaf 1`
+reset=`tput sgr0`
+die() { set +v; echo "$red$*$reset" 1>&2 ; exit 1; }
+
+for SCHEMA in antibodies contributors sample metadata; do
+  for EXAMPLE in examples/tsv-examples/$SCHEMA/*; do
+    echo "Testing $EXAMPLE ..."
+    CMD="src/validate_tsv.py --path $EXAMPLE/$SCHEMA.tsv --schema $SCHEMA --output as_md"
+    echo "( $CMD )"
+    README="$EXAMPLE/README.md"
+    diff $README <( eval "$CMD" ) \
+      || die "Update example: $CMD > $README"
+  done
+done
+
+for GOOD in examples/sample-examples/*/good-*/README.md; do
+  grep 'No errors!' $GOOD > /dev/null || die "$GOOD should not be an error report."
+done
+
+for BAD in examples/sample-examples/*/bad-*/README.md; do
+  ! grep 'No errors!' $BAD || die "$BAD should be an error report."
+done
