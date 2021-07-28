dc9d03931fdf4ccafc12b3b9e46e508538cbcd76
mccalluc <mccalluc@users.noreply.github.com>
2021-07-28 14:56:17 -0400

    pipeline infos in one file

--- src/ingest_validation_tools/pipeline-infos/README.md
+++
@@ -1,2 +0,0 @@
-Joel Welling and Matt Ruffalo have responsibility for the content of this diretory.
-They will keep it up to date, and regenerate documentation after any changes.
--- src/ingest_validation_tools/schema_loader.py
+++ src/ingest_validation_tools/schema_loader.py
@@ -8,12 +8,12 @@ from ingest_validation_tools.yaml_include_loader import load_yaml
 
 _table_schemas_path = Path(__file__).parent / 'table-schemas'
 _directory_schemas_path = Path(__file__).parent / 'directory-schemas'
-_pipeline_infos_path = Path(__file__).parent / 'pipeline-infos'
+_pipeline_infos_path = Path(__file__).parent / 'pipeline-infos/pipeline-infos.yaml'
 
 
 def get_pipeline_infos(name):
-    info_path = _pipeline_infos_path / f'{name}.yaml'
-    return load_yaml(info_path) if info_path.exists() else []
+    infos = load_yaml(_pipeline_infos_path)
+    return infos.get(name, [])
 
 
 class PreflightError(Exception):
