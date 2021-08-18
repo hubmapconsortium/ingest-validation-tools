8ac948544c7ce93743c738df372376a42b9a6d1d
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-08-18 15:59:20 -0400

    explicit python3 (#938)

--- tests/test-doctests.sh
+++ tests/test-doctests.sh
@@ -5,7 +5,7 @@ cd src
 
 DOCTESTS=`find . | grep '\.py$'`
 for TEST in $DOCTESTS; do
-  CMD="python -m doctest $TEST"
+  CMD="python3 -m doctest $TEST"
   # doctest by itself is silent on success.
   echo 'cd src;' $CMD
   $CMD
