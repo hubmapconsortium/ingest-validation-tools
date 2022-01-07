2196592ee767e65c43a39f36b22803592ee171ca
mccalluc <mccalluc@users.noreply.github.com>
2022-01-07 14:04:17 -0500

    plausible Excel file

--- src/generate_grid.py
+++ src/generate_grid.py
@@ -3,17 +3,10 @@
 import argparse
 from pathlib import Path
 import sys
-from yaml import dump as dump_yaml, safe_load
 
-from tableschema_to_template.create_xlsx import create_xlsx
+from yaml import safe_load
+import xlsxwriter
 
-from ingest_validation_tools.schema_loader import (
-    dict_schema_versions, get_table_schema, get_other_schema, get_directory_schema,
-    get_pipeline_infos, get_is_assay, enum_maps_to_lists)
-from ingest_validation_tools.docs_utils import (
-    get_tsv_name, get_xlsx_name,
-    generate_template_tsv, generate_readme_md)
-from ingest_validation_tools.cli_utils import dir_path
 
 
 def main():
@@ -21,7 +14,7 @@ def main():
     parser.add_argument(
         'target',
         type=Path,
-        help='Path for MD file')
+        help='Path for Excel file')
     args = parser.parse_args()
 
     field_schemas = safe_load(
@@ -34,19 +27,16 @@ def main():
     
     schema_cols = list(all_schemas)
 
-    with open(args.target, 'w') as f:
-        f.write(
-'''---
-layout: default
-title: HuBMAP Data Upload Guidelines
----
-''')
-        f.write(' '.join(schema_cols))
-        for field, schemas in field_schemas.items():
-            f.write(field)
-            cols = ['X' if schema in schemas else ' ' for schema in schema_cols]
-            f.write(' '.join(cols))
+    workbook = xlsxwriter.Workbook(args.target)
+    worksheet = workbook.add_worksheet()
 
+    for row, field in enumerate(field_schemas):
+        worksheet.write(row, 0, field)
+        for col, schema in enumerate(schema_cols):
+            if schema in field_schemas[field]:
+                worksheet.write(row, col + 1, 'X')
+
+    workbook.close()
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
