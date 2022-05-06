43cbeab3f9859c7209f11def5bddcb276030f428
mccalluc <mccalluc@users.noreply.github.com>
2022-05-06 12:56:19 -0400

    nested expressions too complicated

--- tests/test-script-docs.sh
+++ tests/test-script-docs.sh
@@ -8,11 +8,14 @@ for TOOL in src/*.py; do
   TOOL=`basename $TOOL`
   echo "Testing $TOOL docs..."
   [ -e src/$TOOL ] || die "src/$TOOL does not exist."
+
   # In python 3.10, this string changed.
   # Since we want the doc build to be the same in all environments, use perl to search and replace.
   DOC_CMD="src/$TOOL -h | perl -pne 's/options:/optional arguments:/'"
-  diff \
-        <(perl -ne 'print if /usage: '$TOOL'/../```/ and ! /```/' $DOCS/README-$TOOL.md) \
-        <($DOC_CMD) \
+
+  perl -ne 'print if /usage: '$TOOL'/../```/ and ! /```/' $DOCS/README-$TOOL.md > /tmp/expected.txt
+  eval $DOC_CMD > /tmp/actual.txt
+
+  diff /tmp/expected.txt /tmp/actual.txt \
       || die 'Update: (echo '"'"'```text'"'"'; '$DOC_CMD'; echo '"'"'```'"'"') >' $DOCS/README-$TOOL.md
 done
