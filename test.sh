29ad47c7872c0773c162644e6933265b7d514eec
mccalluc <mccalluc@users.noreply.github.com>
2022-07-14 10:57:35 -0400

    parallelize test runs: exit on first failure

--- test.sh
+++ test.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit
 
-start() { echo "::group::$1"; }
-end() { echo "::endgroup::"; }
-die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
-
-CONTINUE_FROM="$1"
-
-for TEST in tests/test-*; do
-  if [[ -z $CONTINUE_FROM ]] || [[ $CONTINUE_FROM = $TEST ]]; then
-    CONTINUE_FROM=''
-    start $TEST
-    eval $TEST || die "To continue from here: ./test.sh $TEST"
-    end $TEST
-  fi
-done
+ls tests/* | parallel --halt now,fail=1 bash
