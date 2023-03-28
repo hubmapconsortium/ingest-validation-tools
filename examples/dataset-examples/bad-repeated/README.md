1973ad4c2c2a263f0c45fd98825e0c32b2db9eed
jpuerto-psc <68066250+jpuerto-psc@users.noreply.github.com>
2023-03-28 10:55:48 -0400

    Jpuerto psc/hre (#1175)
    * Table Validator: Get the fields in the schema so that we can reference the examples for regex pattern failures.
    
    * Error Report: Import the new recursive_munge function and use it for as_yaml format.
    
    * Message Munger: Introduce a recursive message munger that can be used by other error reports.
    
    * Check Factory: Modify one of the templates slightly.
    
    * Message Munger: Modify syntax
    
    * General: Modify READMEs so that they match the new errors.
    
    * Message Munger: Simplify recursive_munge significantly and modify substitution to pass test.
    
    * Table Validator: Modify docstring to pass test.
    
    * General: Update README with new errors
    
    * General: Update README with new errors
    
    * General: Update README with new errors
    
    * General: Update CHANGELOG
    
    ---------
    
    Co-authored-by: Juan Puerto <=>

--- examples/dataset-examples/bad-repeated/README.md
+++ examples/dataset-examples/bad-repeated/README.md
@@ -1,6 +1,6 @@
 ```
-Metadata TSV Errors: 'There is more than one TSV for this type: codex'
+Metadata TSV Errors: 'There is more than one TSV for this type: codex.'
 Hint: 'If validation fails because of extra whitespace in the TSV, try:
 
-  src/cleanup_whitespace.py --tsv_in original.tsv --tsv_out clean.tsv'
+  src/cleanup_whitespace.py --tsv_in original.tsv --tsv_out clean.tsv.'
 ```
--- src/ingest_validation_tools/message_munger.py
+++ src/ingest_validation_tools/message_munger.py
@@ -1,5 +1,76 @@
 from re import sub
 
+pat_reps = [
+    (r'constraint "pattern" is (".*")',
+     'it does not match the expected pattern'),
+
+    (r'^Metadata TSV Errors: \S+/',
+     'In '),
+
+    (r'.*: External: (row \d+), data ([^:]+): ([^:]+): (.+)',
+     r'In the dataset \2 referenced on \1, the file "\4" is \3.'),
+
+    (r' \(as \S+\): External: Warning: File has no data rows',
+     r', the file is just a header with no data rows'),
+
+    (r'Reference Errors: Multiple References: (\S+): .*/(.*)',
+     r'The dataset directory "\1" is referenced by \2, '
+     r'and somewhere else, but references should be unique.'),
+
+    (r'Reference Errors: No References: Directories: ',
+     'At the root of an upload, the only directories should be datasets. No TSV references '),
+    (r'Reference Errors: No References: Files: ',
+     'At the root of an upload, the only files should be TSVs. No TSV references '),
+
+    (r'Metadata TSV Errors: Missing: ',
+     ''),
+
+    (r'is not type "(\w+)" and format "default"',
+     r'is not a \1'),
+
+    (r'(,|is) [A-Z]',
+     lambda match: match.group(0).lower()),
+
+    (r'constraint "enum" is "\[(.*)\]"',
+     r'it is not one of these: \1'),
+
+    (r'is no such file or directory',
+     r'does not exist'),
+
+    (r'the file "([^"]+)" is required but missing',
+     r'a file matching "\1" is required but missing'),
+
+    (r'([^.])$',
+     r'\1.'),
+
+    (r'type is "datetime/.*"',
+     r'it is not in the format YYYY-MM-DD Hour:Minute'),
+
+    (r'type is "boolean/default"',
+     r'it is neither true nor false'),
+
+    (r'type is "number/default"',
+     r'it is not in numerical form'),
+
+    (r'type is "integer/default"',
+     r'it is not an integer'),
+
+    (r'URL returned .* "https://dx.doi.org/.*"',
+     r'it is an invalid DOI'),
+
+    (r'(a file matching ".*" is required but missing.)',
+     r'\1 Please review the schema for more information.'),
+
+    (r'\'',
+     '\"'),
+
+    (r'type is "string/email"',
+     r'it is not a valid email'),
+
+    (r'constraint "required" .*',
+     r'it must be filled out.'),
+]
+
 
 def munge(message: str) -> str:
     '''
@@ -9,52 +80,15 @@ def munge(message: str) -> str:
     'In the dataset fake/ds referenced on row 2, the file "nope.txt" is not allowed.'
 
     '''
-    pat_reps = [
-        (r'does not conform to the pattern constraint of .*',
-         'is not the right form.'),
-
-        (r'^Metadata TSV Errors: \S+/',
-         'In '),
-
-        (r'.*: External: (row \d+), data ([^:]+): ([^:]+): (.+)',
-         r'In the dataset \2 referenced on \1, the file "\4" is \3.'),
-
-        (r' \(as \S+\): External: Warning: File has no data rows',
-         r', the file is just a header with no data rows'),
-
-        (r'Reference Errors: Multiple References: (\S+): .*/(.*)',
-         r'The dataset directory "\1" is referenced by \2, '
-         r'and somewhere else, but references should be unique.'),
-
-        (r'Reference Errors: No References: Directories: ',
-         'At the root of an upload, the only directories should be datasets. No TSV references '),
-        (r'Reference Errors: No References: Files: ',
-         'At the root of an upload, the only files should be TSVs. No TSV references '),
-
-        (r'Metadata TSV Errors: Missing: ',
-         ''),
-
-        (r'is not type "integer" and format "default"',
-         r'is not an integer'),
-
-        (r'is not type "(\w+)" and format "default"',
-         r'is not a \1'),
-
-        (r'(,|is) [A-Z]',
-         lambda match: match.group(0).lower()),
-
-        (r'does not conform to the given enumeration: "\[(.*)\]"',
-         r'is not one of these: \1'),
-
-        (r'is no such file or directory',
-         r'does not exist'),
-
-        (r'the file "([^"]+)" is required but missing',
-         r'a file matching "\1" is required but missing'),
-
-        (r'([^.])$',
-         r'\1.')
-    ]
     for pattern, replacement in pat_reps:
         message = sub(pattern, replacement, message)
     return message
+
+
+def recursive_munge(message_collection):
+    if isinstance(message_collection, dict):
+        return {k: recursive_munge(v) for k, v in message_collection.items()}
+    elif isinstance(message_collection, list):
+        return [recursive_munge(v) for v in message_collection]
+    else:
+        return munge(message_collection)
