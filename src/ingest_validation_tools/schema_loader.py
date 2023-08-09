69f46f569516086134dab029a3135073455fa64d
Juan Puerto <=>
2023-08-09 15:34:07 -0400

    General: Syntax fix

--- src/ingest_validation_tools/schema_loader.py
+++ src/ingest_validation_tools/schema_loader.py
@@ -297,7 +297,8 @@ def get_directory_schema(directory_type: str, schema_version: str) -> Optional[d
     schema['files'] += [
         {
             'pattern': r'extras/.*',
-            'description': 'Folder for general lab-specific files related to the dataset. [Exists in all assays]',
+            'description': 'Folder for general lab-specific files related to the dataset. '
+                           '[Exists in all assays]',
             'required': False
         }
     ]
