cd09efb4ee5383e9084c988282ef7336bc304947
Juan Puerto <=>
2024-02-05 10:44:33 -0500

    General: Change error messaging to be more clear about where the error is.

--- src/ingest_validation_tools/upload.py
+++ src/ingest_validation_tools/upload.py
@@ -785,7 +785,7 @@ class Upload:
 
                 for full_path_row_non_global_file, rel_path_row_non_global_file in row_non_global_files.items():
                     if not full_path_row_non_global_file.exists():
-                        errors[str(row_references)] = f"{rel_path_row_non_global_file} not exist in upload."
+                        errors[",".join(row_references)] = f"{rel_path_row_non_global_file} not exist in upload."
         else:
             # Catch case 2
             if self.shared_directories:
