7c20822814c284213d6c42c4a3203c14e13d4348
Gesina Phillips <gesina@psc.edu>
2024-04-11 13:08:16 -0400

    slight changes to doc generation

--- tests/test-generate-docs.sh
+++ tests/test-generate-docs.sh
@@ -32,7 +32,7 @@ for TYPE in $(ls -d docs/*); do
   if [ -e $REAL_DEST/current ] && [ -e $TEST_DEST/current ]; then
     diff -r $REAL_DEST/current $TEST_DEST/current --exclude="*.tsv" --exclude="*.xlsx" \
       || die "Update needed: $REAL_CMD
-  Or:" 'for D in `ls -d docs/*/`; do D=`basename $D`; src/generate_docs.py $D docs/$D; done'
+  Or:" 'for D in `ls -d docs/*/`; do D=`basename $D`|| continue; src/generate_docs.py $D docs/$D; echo $D; done'
   fi
 
   if [ -e $REAL_DEST/deprecated ] && [ -e $TEST_DEST/deprecated ]; then
