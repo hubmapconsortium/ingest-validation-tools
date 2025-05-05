2c5490eae3e349c62bdc5da86a2a7470e309f3f0
Gesina Phillips <gesina@psc.edu>
2025-05-05 16:35:33 -0400

    rollback metadata_tsv param change

--- src/ingest_validation_tools/plugin_validator.py
+++ src/ingest_validation_tools/plugin_validator.py
@@ -53,7 +53,7 @@ class Validator(object):
         assay_type: str,
         contains: List = [],
         verbose: bool = False,
-        schema: Optional[SchemaVersion] = None,
+        metadata_tsv: Optional[SchemaVersion] = None,
         globus_token: str = "",
         app_context: Dict[str, str] = {},
         **kwargs,
@@ -75,7 +75,7 @@ class Validator(object):
         self.assay_type = assay_type
         self.contains = contains
         self.verbose = verbose
-        self.schema = schema
+        self.metadata_tsv = metadata_tsv
         self.token = globus_token
         self.app_context = app_context
 
@@ -148,7 +148,7 @@ def run_plugin_validators_iter(
                 plugin_dir,
                 sv.contains,
                 verbose=verbose,
-                schema=sv,
+                metadata_tsv=sv,
                 globus_token=globus_token,
                 app_context=app_context,
                 **kwargs,
@@ -196,7 +196,7 @@ def validation_error_iter(
     plugin_dir: PathOrStr,
     contains: List,
     verbose: bool = False,
-    schema: Optional[SchemaVersion] = None,
+    metadata_tsv: Optional[SchemaVersion] = None,
     globus_token: str = "",
     app_context: Dict[str, str] = {},
     **kwargs,
@@ -215,7 +215,9 @@ def validation_error_iter(
     error messages
     """
     for cls in validation_class_iter(plugin_dir):
-        validator = cls(paths, assay_type, contains, verbose, schema, globus_token, app_context)
+        validator = cls(
+            paths, assay_type, contains, verbose, metadata_tsv, globus_token, app_context
+        )
         kwargs["verbose"] = verbose
         for err in validator.collect_errors(**kwargs):
             yield cls, err
