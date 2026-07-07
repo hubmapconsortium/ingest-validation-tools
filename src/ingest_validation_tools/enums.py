2089251d7a71c9dde1ca016672e2e891300615d6
Juan Muerto <jpuerto@psc.edu>
2026-07-07 12:25:56 -0400

    chore: syntax

--- src/ingest_validation_tools/enums.py
+++ src/ingest_validation_tools/enums.py
@@ -199,7 +199,7 @@ shared_enums: dict[str, list[str]] = {
         "COMET",
         "DNA Methylation",
         "DICOM-MRI",
-        "Paired-Tag"
+        "Paired-Tag",
     ],
     "analyte_class": [
         "DNA",
